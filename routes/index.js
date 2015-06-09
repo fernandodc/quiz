2c1da6870684c0480068592c86133e9fab2fb9e7
fernandodc <fernandodecaminos@gmail.com>
2015-06-09 14:15:47 -0300

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var quizController=require('../controllers/quiz_controller');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'quiz' });
 });
+
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
